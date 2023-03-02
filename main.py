5ebffc7ad8aa9f02930d32f8198baf4cb6de4a38
Kotlyarova04 <p.kotlyarova@g.nsu.ru>
2023-03-02 11:29:54 +0700

    hahha

--- main.py
+++ main.py
@@ -6,3 +6,37 @@ Rafaevich Vita
 Leonov Kirill
 '''
 
+def triangle(x, y, a, b, angle, color):
+    '''
+    Function for drawing triangle.
+    :param x:
+    :param y:
+    :param a:
+    :param b:
+    :param angle:
+    :param color:
+    :return: None
+    '''
+    pass
+
+
+def draw_dog():
+    '''Drawing big dog. Rrrrrr!'''
+    pass
+
+
+def draw_cat():
+    pass
+
+
+def main():
+    '''
+    Maim function.
+    :return:
+    '''
+    draw_cat()
+    draw_dog()
+
+
+if __name__ == '__main__':
+    main()
