d4bb3fb99d0de8bf5c794dc0b61f5bfc8acdd84b
LeonoffNSU <k.leonov@g.nsu.ru>
2023-03-09 12:11:03 +0700

    pattern

--- kirill.py
+++ kirill.py
@@ -2,7 +2,6 @@ import turtle
 import math
 if __name__=='__main__':
     turtle.setup(width=1.0, height=1.0)
-    turtle.speed(100)
     turtle.pensize(5)
     turtle.pu()
     turtle.goto(-255,-400)
@@ -17,11 +16,6 @@ if __name__=='__main__':
 
 #510 пикселей - одна треть
 #765 пикселей - одна половина
-
-def square(x):
-    for _ in range(0,4):
-        turtle.fd(x)
-        turtle.rt(90)
 
 def ellipsoid(bigrad,littlerad,deg):
     '''
@@ -39,98 +33,62 @@ def ellipsoid(bigrad,littlerad,deg):
         y = -littlerad * math.cos(rad) + littlerad + dy
         turtle.goto(x,y)
 
-bigrad, littlerad = 60, 30
-standard = 80
+def pattern(change1,change2,change3,change4,change5,standard):
+
+    turtle.rt(90)
+    turtle.fd(standard)
+    turtle.circle(standard//4,90)
+    turtle.fd(standard)
+    turtle.pu()
+    turtle.goto(turtle.xcor() - change1, turtle.ycor() - change2)
+    turtle.pd()
+
+    ellipsoid(change1,change2,270)
+    turtle.rt(180)
+    turtle.fd(abs(change2))
+    turtle.lt(90)
+    turtle.fd(abs(change1))
+    turtle.rt(90)
+    turtle.fd(standard//8)
+    turtle.rt(90)
+    turtle.fd(abs(change1) + 40)
+    turtle.pu()
+    turtle.goto(turtle.xcor(),turtle.ycor() - change3)     #120
+    turtle.pd()
+
+    ellipsoid(change2,change1,360)
+    turtle.lt(180)
+    turtle.fd(standard//8*3)
+    turtle.lt(90)
+    turtle.fd(standard//8*3)
+    turtle.circle(-standard//8*3,90)
+    turtle.end_fill()
+    turtle.pu()
+
+    turtle.goto(turtle.xcor() + change4,turtle.ycor() + change5)    #change4, change5 = 5, 85
+    turtle.pd()
+    turtle.fillcolor('white')
+    turtle.begin_fill()
+    turtle.circle(15)
+    turtle.end_fill()
+    turtle.pu()
+
+change1, change2, change3, change4, change5, standard = 60, 30, 120, 5, 85, 80
 
 turtle.goto(-550,5)
 turtle.pensize(2)
+turtle.pencolor('Cornflowerblue')
 turtle.fillcolor('Cornflowerblue')
 turtle.begin_fill()
 turtle.pd()
-turtle.pencolor('Cornflowerblue')
 
+pattern(change1, change2, change3, change4, change5, standard)
+turtle.goto(turtle.xcor() + 75, turtle.ycor() - 95)
 turtle.rt(90)
-turtle.fd(standard)
-turtle.circle(standard//4,90)
-turtle.fd(standard)
-turtle.pu()
-turtle.goto(turtle.xcor() - bigrad,turtle.ycor() - littlerad)
-turtle.pd()
 
-
-ellipsoid(bigrad,littlerad,270)
-turtle.rt(180)
-turtle.fd(littlerad)
-turtle.lt(90)
-turtle.fd(bigrad)
-turtle.rt(90)
-turtle.fd(standard//8)
-turtle.rt(90)
-turtle.fd(bigrad + 40)
-turtle.pu()
-turtle.goto(turtle.xcor(),turtle.ycor() - 120)
-turtle.pd()
-
-ellipsoid(littlerad,bigrad,360)
-turtle.lt(180)
-turtle.fd(standard//8*3)
-turtle.lt(90)
-turtle.fd(standard//8*3)
-turtle.circle(-standard//8*3,90)
-turtle.end_fill()
-turtle.pu()
-
-turtle.goto(turtle.xcor() + 5,turtle.ycor() + 85)
-turtle.pd()
-turtle.fillcolor('white')
-turtle.begin_fill()
-turtle.circle(15)
-turtle.end_fill()
-turtle.pu()
-turtle.goto(turtle.xcor() + 75,turtle.ycor() - 95)
-turtle.lt(180)
-
-# вторая часть кода
+turtle.pencolor('DarkGoldenrod1')
 turtle.fillcolor('DarkGoldenrod1')
 turtle.begin_fill()
 turtle.pd()
-turtle.pencolor('DarkGoldenrod1')
-
-turtle.fd(standard)
-turtle.circle(standard//4,90)
-turtle.fd(standard)
-turtle.pu()
-turtle.goto(turtle.xcor() + bigrad,turtle.ycor() + littlerad)  # кажется, это важно
-turtle.pd()
-
-
-ellipsoid(-bigrad,-littlerad,270)    # важно
-turtle.rt(180)
-turtle.fd(littlerad)
-turtle.lt(90)
-turtle.fd(bigrad)
-turtle.rt(90)
-turtle.fd(standard//8)
-turtle.rt(90)
-turtle.fd(bigrad + 40)
-turtle.pu()
-turtle.goto(turtle.xcor(),turtle.ycor() + 120)     #???
-turtle.pd()
-
-ellipsoid(-littlerad,-bigrad,360)     #важно
-turtle.lt(180)
-turtle.fd(standard//8*3)
-turtle.lt(90)
-turtle.fd(standard//8*3)
-turtle.circle(-standard//8*3,90)
-turtle.end_fill()
-turtle.pu()
-
-turtle.goto(turtle.xcor() - 5,turtle.ycor() - 85)    #важно
-turtle.pd()
-turtle.fillcolor('white')
-turtle.begin_fill()
-turtle.circle(15)
-turtle.end_fill()
-turtle.pu()
+pattern(-change1, -change2, -change3, -change4, -change5, standard)
 turtle.done()
